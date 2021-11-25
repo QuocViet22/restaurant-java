5d26575d6403cc4380b92b2880adf37bdad5b250
Your Name <your@example.com>
2021-11-25 15:45:09 +0700

    viet

---
+++ build/generated-sources/ap-source-output/com/business/Invoice_.java
@@ -0,0 +1,16 @@
+package com.business;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-11-20T08:32:13")
+@StaticMetamodel(Invoice.class)
+public class Invoice_ { 
+
+    public static volatile SingularAttribute<Invoice, Integer> stt;
+    public static volatile SingularAttribute<Invoice, String> productQuantity;
+    public static volatile SingularAttribute<Invoice, String> time;
+    public static volatile SingularAttribute<Invoice, String> productName;
+
+}
